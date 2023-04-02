c783c0151d6bd34e0788134816f1383a550af06d
Adrian Zinko <adrian.zinko@boldare.com>
2023-04-02 19:19:25 +0200

    force ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { db } from '@/db/db';
 import { countries } from '@/db/schema';
 
-const inter = Inter({ subsets: ['latin'] })
+export const dynamic = "force-dynamic";
 
 export default async function  Home() {
   const data = await db.select().from(countries);
-
-  console.log(data);
-  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
